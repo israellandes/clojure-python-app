b3425ac6f0372ff77bba8b99a97fc86a4bb3c732
Is-Rael <isreal.landes9@gmail.com>
2023-02-28 11:36:43 -0800

    yo

--- python-lib/setup.py
+++ python-lib/setup.py
@@ -1,11 +1,21 @@
-from setuptools import find_packages, setup
+import setuptools
 
-setup(
-    name="mypythonlib",
-    packages=find_packages(include=["mypythonlib"]),
-    version="0.1.0",
-    description="My first Python library",
-    author="Me",
-    license="MIT",
-    install_requires=[],
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="demopackage",
+    version="1.0.0",
+    author="demo_author",
+    author_email="demo@email.com",
+    description="A small demo package",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/username/demopackage",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ),
 )
